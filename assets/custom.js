544b9f37af619b4da6583e8c17ae9513657d49a5
bigbit99 <bigbit831@gmail.com>
2023-02-26 23:33:17 +0900

    tab text animation add

--- assets/custom.js
+++ assets/custom.js
@@ -1,24 +1,4 @@
 //section6 - 탭 컨트롤
-// const tabItem = document.querySelectorAll('.tab-container__item');
-// const tabContent = document.querySelectorAll('.content-container__content');
-
-// tabItem.forEach((item) => {
-//   item.addEventListener('click', tabHandler);
-// });
-
-// function tabHandler(item) {
-//   const tabTarget = item.currentTarget;
-//   const target = tabTarget.dataset.tab;
-//   tabItem.forEach((title) => {
-//     title.classList.remove('active');
-//   });
-//   tabContent.forEach((target) => {
-//     target.classList.remove('target');
-//   });
-//   document.querySelector('#' + target).classList.add('target');
-//   tabTarget.classList.add('active');
-// }
-
 const tabList = document.querySelectorAll('.tabWrap .list li');
 const contents = document.querySelectorAll('.works_img .tabCont');
 let activeCont = '';
@@ -28,10 +8,12 @@ for (let i = 0; i < tabList.length; i++) {
     e.preventDefault();
     for (let j = 0; j < tabList.length; j++) {
       tabList[j].classList.remove('active');
-      contents[j].style.display = 'none';
+      contents[j].classList.remove('activeView');
+      // contents[j].style.display = 'none';
     }
     this.parentNode.classList.add('active');
     activeCont = this.getAttribute('href');
-    document.querySelector(activeCont).style.display = 'block';
+    document.querySelector(activeCont).classList.add('activeView');
+    // document.querySelector(activeCont).style.display = 'block';
   });
 }
